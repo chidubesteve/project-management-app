a3581fb5f73fd88b05ccb30abb67ceb517fb984f
chidube anike <anikechidube@gmail.com>
2024-11-14 11:30:26 +0100

    added new task modal

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -1,33 +1,47 @@
-"use client"
+"use client";
 
-import React, { useState } from 'react'
-import ProjectHeader from '../ProjectHeader'
-import Board from '../BoardView'
-import List from '../ListView'
-import Timeline from '../Timeline'
-import Table from '../TableView'
+import React, { useState } from "react";
+import ProjectHeader from "../ProjectHeader";
+import Board from "../BoardView";
+import List from "../ListView";
+import Timeline from "../Timeline";
+import Table from "../TableView";
+import ModalNewTask from "@/components/ModalNewTask";
 
 type Props = {
-    params: {
-        id: string
-    }
-}
+  params: {
+    id: string;
+  };
+};
 
 const Project = ({ params }: Props) => {
-    const { id } = params
-    const [activeTab, setActiveTab] = useState("Board");
-    const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false)
+  const { id } = params;
+  const [activeTab, setActiveTab] = useState("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
 
   return (
-      <div>
-          {/* new task */}
-          <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
-          {activeTab === "Board" && <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />}
-          {activeTab === "List" && <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />}
-          {activeTab === "Timeline" && <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />}
-          {activeTab === "Table" && <Table id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />}
+    <div>
+      {/* new task */}
+      <ModalNewTask
+        isOpen={isModalNewTaskOpen}
+        onClose={() => setIsModalNewTaskOpen(false)}
+        id={id}
+      />
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === "Board" && (
+        <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "List" && (
+        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Timeline" && (
+        <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Table" && (
+        <Table id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
