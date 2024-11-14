e547a40950fe06642dd7da81497c105606758f6a
chidube anike <anikechidube@gmail.com>
2024-11-14 13:54:22 +0100

    added inherit to use customized color instead of  default blue

--- client/src/components/DataFetching/FetchingState.tsx
+++ client/src/components/DataFetching/FetchingState.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const FetchingState = () => {
   return (
     <div className="flex h-full w-full items-center justify-center">
-      <CircularProgress size={"3.5rem"} className='text-green-500 dark:text-green-600' />
+      <CircularProgress size={"3.5rem"} className='text-green-500 dark:text-green-600' color="inherit" />
     </div>
   );
 }
