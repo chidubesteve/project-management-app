f05abfdda4d3f8a818804d3b59a15adb6d4fd37d
chidube anike <anikechidube@gmail.com>
2024-11-26 18:03:45 +0100

    removed null if no current user

--- client/src/components/NavBar/index.tsx
+++ client/src/components/NavBar/index.tsx
@@ -25,7 +25,7 @@ const NavBar = () => {
     }
   };
 
-  if (!currentUser) return null;
+  // if (!currentUser) return null;
   const currentUserDetails = currentUser?.userDetails;
   return (
     <div
